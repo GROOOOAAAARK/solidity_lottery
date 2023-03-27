a490d13562513a2d3a43a5e19bb79b1fff849bda
GROARK <grk_010@protonmail.com>
2023-03-27 07:20:59 +0200

    chore: add config files

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ganache";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  // networks: {
+    // ganache: {
+    //   url: "http://localhost:7545",
+    // },
+    // sepolia: {
+    //   url: "https://rpc.sepolia.com",
+    //   accounts: {
+    //     mnemonic: process.env.MNEMONIC,
+    //   },
+    // },
+  // },
+};
+
+export default config;
