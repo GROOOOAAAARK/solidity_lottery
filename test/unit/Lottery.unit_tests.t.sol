50c72513a54aaead8b7492fe530f7fdebbc47ff6
GRK <gitkey.aluminum361@passmail.net>
2025-10-23 12:51:32 +0200

    test: improve lottery tests cases with 2 users

--- test/unit/Lottery.unit_tests.t.sol
+++ test/unit/Lottery.unit_tests.t.sol
@@ -8,7 +8,8 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract LotteryUnitTestsLocal is Script {
     Lottery lotteryContract;
-    address _user;
+    address _user1;
+    address _user2;
     address _owner;
 
     function setUp() public {
@@ -22,20 +23,24 @@ contract LotteryUnitTestsLocal is Script {
         address payable lotteryAddress = deployer._deploy(10, 3, _owner);
         lotteryContract = Lottery(lotteryAddress);
 
-        uint256 userPvK = vm.deriveKey(mnemonic, 1);
-        _user = vm.addr(userPvK);
+        uint256 user1PvK = vm.deriveKey(mnemonic, 1);
+        _user1 = vm.addr(user1PvK);
+
+        uint256 user2PvK = vm.deriveKey(mnemonic, 2);
+        _user2 = vm.addr(user2PvK);
 
         vm.deal(_owner, 1000);
-        vm.deal(_user, 1000);
+        vm.deal(_user1, 1000);
+        vm.deal(_user2, 1000);
 
-        console.log("Owner: %s (default active user) --- User: %s\n\n", _owner, _user);
+        console.log("Owner: %s (default active user1) --- User: %s\n\n", _owner, _user1);
     }
 
     // @dev Start Lottery conditions
     function testStartLottery() public {
         // Test nobody can start the lottery except the owner
-        vm.prank(_user);
-        vm.expectRevert("Ownable: caller is not the owner"); //"OwnableUnauthorizedAccount(%s)", _user
+        vm.prank(_user1);
+        vm.expectRevert("Ownable: caller is not the owner"); //"OwnableUnauthorizedAccount(%s)", _user1
         lotteryContract.startLottery();
         vm.stopPrank();
 
@@ -56,8 +61,8 @@ contract LotteryUnitTestsLocal is Script {
         uint256 newTicketPrice = 10;
         uint256 newMaxTicketCount = 10;
 
-        // Invalid user permissions
-        vm.prank(_user);
+        // Invalid user1 permissions
+        vm.prank(_user1);
         vm.expectRevert("Ownable: caller is not the owner");
         lotteryContract.resetLottery(newTicketPrice, newMaxTicketCount);
         vm.stopPrank();
@@ -80,7 +85,7 @@ contract LotteryUnitTestsLocal is Script {
         - [ ] Test ok
         - [ ] Test not enought msg value
         - [ ] Test for endLottery and get random winner
-        - [ ] Te
+        // - [ ] Te
     */
     function testBuyTicket() public {
 
@@ -89,11 +94,36 @@ contract LotteryUnitTestsLocal is Script {
         vm.stopPrank();
 
         // Test buy function OK
-        vm.startPrank(_user);
+        vm.startPrank(_user1);
         lotteryContract.buyTicket{value: lotteryContract.ticketPrice()}();
         assert(lotteryContract.ticketsSold() == 1);
         assert(lotteryContract.ticketsLeft() == lotteryContract.maxTicketCount() - 1);
         assert(address(lotteryContract).balance == lotteryContract.ticketPrice() * 1);
 
+        // Test buy wrong value too low
+        uint256 badValueLow = lotteryContract.ticketPrice() - 1;
+        vm.expectRevert("Lottery: invalid ticket price");
+        lotteryContract.buyTicket{value: badValueLow}();
+
+        // Test buy wrong value too high
+        uint256 badValueHigh = lotteryContract.ticketPrice() + 1;
+        vm.expectRevert("Lottery: invalid ticket price");
+        lotteryContract.buyTicket{value: badValueHigh}();
+
+        vm.stopPrank();
+
+        vm.prank(_user2);
+        lotteryContract.buyTicket{value: lotteryContract.ticketPrice()}();
+        assert(lotteryContract.ticketsSold() == 2);
+        assert(lotteryContract.ticketsLeft() == lotteryContract.maxTicketCount() - 2);
+        assert(address(lotteryContract).balance == lotteryContract.ticketPrice() * 2);
+
+        vm.stopPrank();
+
+        vm.prank(_owner);
+        lotteryContract.buyTicket{value: lotteryContract.ticketPrice()}();
+        assert(lotteryContract.ticketsSold() == 3);
+        assert(lotteryContract.ticketsLeft() == lotteryContract.maxTicketCount() - 3);
+        assert(lotteryContract.state() == Lottery.State.Ended);
     }
 }
